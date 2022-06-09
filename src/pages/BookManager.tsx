41e5c28d82b239d0fe3e20806742cdc5b26a8827
Cookies <souyuuki12@gmail.com>
2022-06-10 05:04:02 +0800

    Updated Book Manager page

--- src/pages/BookManager.tsx
+++ src/pages/BookManager.tsx
@@ -1,11 +1,51 @@
+import React, {
+  useEffect,
+  useState
+} from 'react';
+
+import Table from '../components/Table';
+
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+
+import { selectBookList } from '../reducers/bookSlice';
+
 import './BookManager.css'
 
 const BookManager = (props:any): JSX.Element => {
+  const dispatch = useAppDispatch()
+  const bookList = useAppSelector(selectBookList)
+
+  const data = React.useMemo(() => [
+    {
+      col1: 'Hello',
+      col2: 'World',
+    },
+    {
+      col1: 'react-table',
+      col2: 'rocks',
+    },
+    {
+      col1: 'whatever',
+      col2: 'you want',
+    },
+  ], [])
+  
+  const columns = React.useMemo(() => [
+    {
+      Header: 'Column 1',
+      accessor: 'col1', // accessor is the "key" in the data
+    },
+    {
+      Header: 'Column 2',
+      accessor: 'col2',
+    },
+  ], [])
+
   return (
     <>
       <main className="container">
         <h2>Welcome to the Book Manager!</h2>
-        <p>You can do this, I believe in you.</p>
+        <Table columns={columns} data={data} />
       </main>
     </>
   );
