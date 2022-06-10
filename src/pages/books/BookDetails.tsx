40242c77c12bd7047fa6c2c6920478c5bad21b75
Cookies <souyuuki12@gmail.com>
2022-06-11 05:33:56 +0800

    Added books details page

---
+++ src/pages/books/BookDetails.tsx
@@ -0,0 +1,12 @@
+const BookDetails = (props:any): JSX.Element => {
+  return (
+    <>
+      <div className="container">
+        <h2>Welcome to the Analytics!</h2>
+        <p>You can do this, I believe in you.</p>
+      </div>
+    </>
+  );
+}
+
+export default BookDetails;
