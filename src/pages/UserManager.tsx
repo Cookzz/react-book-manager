0331aadb7ffc3e224c7f659885da286f6939eae9
Cookies <souyuuki12@gmail.com>
2022-06-12 06:21:11 +0800

    Updated user table

--- src/pages/UserManager.tsx
+++ src/pages/UserManager.tsx
@@ -1,9 +1,38 @@
+import React, {
+  useEffect,
+  useState,
+  useMemo
+} from 'react';
+
+import CustomTable from '../components/CustomTable';
+
+import { createRowData, createColumnData } from '../utils/FormatUtils';
+
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+
+import { selectUserList } from '../reducers/userSlice';
+
+import './BookManager.css'
+
 const UserManager = (props:any): JSX.Element => {
+  const dispatch = useAppDispatch()
+  const userList = useAppSelector(selectUserList)
+  const filterList = ['id', 'password']
+
+  const rows = useMemo(() => createRowData(userList, filterList), [userList, filterList])
+  
+  const columns = useMemo(() => createColumnData(userList, filterList), [userList, filterList])
+
   return (
     <>
       <main className="container">
         <h2>Welcome to the User Manager!</h2>
-        <p>You can do this, I believe in you.</p>
+        <CustomTable 
+          title={'Users'}
+          rows={rows} 
+          columns={columns}
+          data={userList}
+        />
       </main>
     </>
   );
