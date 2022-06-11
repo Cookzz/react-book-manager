58ea36490699ca62cf59cff7acd6353eca28b3cb
Cookies <souyuuki12@gmail.com>
2022-06-12 06:21:46 +0800

    Updated misc
    1. Added new packages
    2. Updated routes
    3. Updated util functions

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,46 @@ import { Routes, Route, Navigate } from "react-router-dom";
 
 import NavigationBar from './components/NavigationBar';
 
+import Login from './pages/Login'
+
 import BookManager from './pages/BookManager';
 import Analytics from './pages/Analytics';
 import UserManager from './pages/UserManager';
+import BookDetails from './pages/books/BookDetails';
+
+import { selectUser } from './reducers/userSlice'
+
+import { useAppSelector } from './redux/hooks'
+
+const LoginPage = () => (
+  <Routes>
+    <Route path="/login" element={<Login />} />
+    <Route path="*" element={<Navigate to="/login" replace />} />
+  </Routes>
+)
+
+const Home = () => (
+  <>
+    <NavigationBar />
+    <Routes>
+      <Route path="books" element={<BookManager />} />
+      <Route path="books/create" element={<BookDetails />} />
+      <Route path="analytics" element={<Analytics />} />
+      <Route path="users" element={<UserManager />} />
+      <Route path="*" element={<Navigate to="/books" replace />} />
+    </Routes>
+  </>
+)
 
 const App = () => {
+  const user = useAppSelector(selectUser)
+
   return (
     <div>
-      <NavigationBar />
-      <Routes>
-        <Route path="books" element={<BookManager />} />
-        <Route path="analytics" element={<Analytics />} />
-        <Route path="users" element={<UserManager />} />
-        <Route path="*" element={<Navigate to="/books" replace />} />
-      </Routes>
+      { user ? 
+        <Home /> : 
+        <LoginPage />
+      }
     </div>
   );
 }
--- src/utils/FormatUtils.tsx
+++ src/utils/FormatUtils.tsx
@@ -1,27 +1,38 @@
-import { Book } from "../reducers/bookSlice";
-
-export const createColumnData = (data: Array<Book>) => {
-  if (data.length == 0){
+export const createColumnData = (data: Array<Object>, filterList: Array<string> = []) => {
+  if (data.length === 0){
     return []
   }
 
   const rowData = data[0];
 
-  const dataColumns = Object.keys(rowData).map((key, i)=>({
+  let columns = Object.keys(rowData).map((key, i)=>({
     id: key,
     label: (key.replace(/_/g, ' ')).toUpperCase(),
     disablePadding: (i === 0),
     numeric: false
   }))
 
-  const extraColumns = [{
-    id: "update",
-    label: "Update".toUpperCase(),
-    disablePadding: false,
-    numeric: false
-  }]
-
-  const columns = [...dataColumns, ...extraColumns]
+  if (filterList.length > 0){
+    columns = columns.filter((column: any) => !filterList.includes(column.id))
+  }
 
   return columns;
+}
+
+export const createRowData = (data: Array<Object>, filterList: Array<string> = []) => {
+  if (data.length === 0){
+    return []
+  }
+
+  let rows = data;
+
+  if (filterList.length > 0){
+    rows = data.map((row)=>{
+      const newRow = Object.fromEntries(Object.entries(row).filter(([key]) => !filterList.includes(key)));
+
+      return newRow;
+    })
+  }
+
+  return rows;
 }
